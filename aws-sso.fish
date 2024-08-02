7fa6ae915aee9153b90106848e9e68f7ff34e9c9
Remi Salmon <salmon.remi@gmail.com>
2024-08-02 10:44:45 -0600

    Update aws-sso script

--- aws-sso.fish
+++ aws-sso.fish
@@ -7,26 +7,15 @@ end
 
 set AWS_PROFILE $argv[1]
 
-aws sso login --profile $AWS_PROFILE || exit 1
+if not aws sts get-caller-identity &> /dev/null
+    aws sso login --profile $AWS_PROFILE || exit 1
+end
 
-set json_web_token (jq '.' (ls -t ~/.aws/sso/cache/*.json | head -n 1))
+set credentials (aws configure export-credentials --profile $AWS_PROFILE)
 
-set AWS_REGION       (echo $json_web_token | jq -r '.region')
-set AWS_ACCESS_TOKEN (echo $json_web_token | jq -r '.accessToken')
-set AWS_ROLE_NAME    (aws configure get sso_role_name --profile $AWS_PROFILE)
-set AWS_ACCOUNT_ID   (aws configure get sso_account_id --profile $AWS_PROFILE)
+set -Ux AWS_REGION            (aws configure get region --profile $AWS_PROFILE)
+set -Ux AWS_ACCESS_KEY_ID     (echo $credentials | jq -r '.AccessKeyId')
+set -Ux AWS_SECRET_ACCESS_KEY (echo $credentials | jq -r '.SecretAccessKey')
+set -Ux AWS_SESSION_TOKEN     (echo $credentials | jq -r '.SessionToken')
 
-set role_credentials (
-    aws sso get-role-credentials --region $AWS_REGION \
-                                 --role-name $AWS_ROLE_NAME  \
-                                 --account-id $AWS_ACCOUNT_ID \
-                                 --access-token $AWS_ACCESS_TOKEN \
-    | jq '.roleCredentials'
-)
-
-set -Ux AWS_REGION            $AWS_REGION
-set -Ux AWS_ACCESS_KEY_ID     (echo $role_credentials | jq -r '.accessKeyId')
-set -Ux AWS_SECRET_ACCESS_KEY (echo $role_credentials | jq -r '.secretAccessKey')
-set -Ux AWS_SESSION_TOKEN     (echo $role_credentials | jq -r '.sessionToken')
-
-echo "AWS SSO role credentials set. Expiration: "(echo $json_web_token | jq -r '.expiresAt')
+echo "AWS credentials exported. Expiration: "(echo $credentials | jq -r '.Expiration')
