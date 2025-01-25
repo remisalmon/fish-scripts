191988e1f080dfd01b8211fa4cc85f43173b98f1
Remi Salmon <salmon.remi@gmail.com>
2025-01-24 21:14:08 -0700

    Update aws-sso script

--- aws-sso.fish
+++ aws-sso.fish
@@ -8,14 +8,14 @@ end
 set AWS_PROFILE $argv[1]
 
 if not aws sts get-caller-identity &> /dev/null
-    aws sso login --profile $AWS_PROFILE || exit 1
+    aws sso login --profile $AWS_PROFILE
 end
 
-set credentials (aws configure export-credentials --profile $AWS_PROFILE)
+set credentials (aws configure export-credentials --profile $AWS_PROFILE) ||  exit 1
 
 set -Ux AWS_REGION            (aws configure get region --profile $AWS_PROFILE)
 set -Ux AWS_ACCESS_KEY_ID     (echo $credentials | jq -r '.AccessKeyId')
 set -Ux AWS_SECRET_ACCESS_KEY (echo $credentials | jq -r '.SecretAccessKey')
 set -Ux AWS_SESSION_TOKEN     (echo $credentials | jq -r '.SessionToken')
 
-echo "AWS credentials exported. Expiration: "(echo $credentials | jq -r '.Expiration')
+echo -e "\nAWS credentials exported. Expiration: "(echo $credentials | jq -r '.Expiration')"\n"
