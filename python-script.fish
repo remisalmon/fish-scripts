1041f4cf2443c4915f6c82064d76f76c5bdf3fac
Remi Salmon <salmon.remi@gmail.com>
2025-02-18 21:50:41 -0700

    Add python-script script

---
+++ python-script.fish
@@ -0,0 +1,31 @@
+#!/usr/bin/env fish
+
+if test (count $argv) -eq 0
+    set file "script.py"
+else
+    set file $argv[1]
+end
+
+if test -e $file
+    echo $file" already exists"
+    exit 1
+end
+
+echo -n "\
+#!/usr/bin/env python
+
+# imports
+
+# globals
+
+# functions
+
+# classes
+
+# main
+def main():
+    return
+
+if __name__ == \"__main__\":
+    main()
+" > $file
