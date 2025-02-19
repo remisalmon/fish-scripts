40d2fd269b89faf6648b09000f6dda3477c5572b
Remi Salmon <salmon.remi@gmail.com>
2025-02-18 21:50:50 -0700

    Add ssh-forward script

---
+++ ssh-forward.fish
@@ -0,0 +1,28 @@
+#!/usr/bin/env fish
+
+if test (count $argv) -ne 2
+    echo "usage: ssh-forward.fish host port"
+
+    exit 1
+end
+
+function kill_exit -s INT
+    kill -s KILL $ssh_pid
+
+    exit 0
+end
+
+set -l host $argv[1]
+set -l port $argv[2]
+
+ssh -N -L {$port}:localhost:{$port} $host &
+
+set -g ssh_pid $last_pid
+
+sleep 1s
+
+open http://localhost:{$port}
+
+while true
+    sleep 1s
+end
