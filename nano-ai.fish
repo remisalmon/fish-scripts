ee218076f7d756e1f5bbf4d358fe1e45ef844db0
Remi Salmon <salmon.remi@gmail.com>
2025-06-30 15:14:53 -0600

    Update nano-ai script

--- nano-ai.fish
+++ nano-ai.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
-set model "gemini-2.0-flash"
-# set model "gemini-2.5-pro-preview-05-06"
+set model "gemini-2.5-flash"
+# set model "gemini-2.5-pro"
 
 set system_instruction "You are a code assistant for a text editor, return only a single code block without examples or explanations."
 
@@ -31,5 +31,5 @@ end
 echo $response | jq -r '.candidates[0].content.parts[0].text' | string collect | head -n -1 | tail -n +2
 
 duckdb (status dirname)/nano-ai.db \
-    -c "create table if not exists logs (created_at timestamp default current_timestamp, prompt varchar, response json);" \
-    -c "insert into logs (prompt, response) values(\$\$"$prompt"\$\$, \$\$"$response"\$\$);" &
+    -c "create table if not exists logs (created_at timestamp default current_timestamp, model varchar, prompt varchar, response json);" \
+    -c "insert into logs (model, prompt, response) values(\$\$"$model"\$\$, \$\$"$prompt"\$\$, \$\$"$response"\$\$);" &
