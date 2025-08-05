8b273d2fe0680f2db2c94704e0149fe75fdd6ae0
Remi Salmon <salmon.remi@gmail.com>
2025-08-05 14:40:24 -0600

    Update git-open script

--- git-open.fish
+++ git-open.fish
@@ -1,3 +1,36 @@
 #!/usr/bin/env fish
 
-nano (git ls-files | string match -m 1 -e $argv[1] || true)
+if not git rev-parse
+    exit 1
+end
+
+set log "/tmp/git-open-"(basename (git rev-parse --show-toplevel))".log"
+
+# files with name match
+set files (git ls-files | string match -i -e (string join "*" $argv))
+
+# files with content match
+if test (count $files) -eq 0
+    set files (git grep -i -l (string join ".*" $argv))
+end
+
+# files with no match
+if test (count $files) -eq 0
+    exit 1
+end
+
+if not test -e $log
+or test (git log --oneline --max-count=1 | string split -f 1 " ") != (head -n 1 $log | string split -f 1 " ")
+    git log --oneline --name-only > $log
+end
+
+set data
+
+for file in $files
+    set n (string match -g -r "(^|/)"(basename $file) < $log | count) # is (grep -E "(^|/)"(basename $file) $log | count)
+    set -a data $n" "$file
+end
+
+echo -s -n $data\n | sort -n & # debug
+
+nano (echo -s -n $data\n | sort -n -r | head -n 1 | string split -f 2 " ")
