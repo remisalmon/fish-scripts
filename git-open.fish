f2d08888af33b4bd435e2f4fcfd45b34a6058381
Remi Salmon <salmon.remi@gmail.com>
2025-08-29 11:03:35 -0600

    Update git-open script

--- git-open.fish
+++ git-open.fish
@@ -20,18 +20,23 @@ if test (count $files) -eq 0
     exit 1
 end
 
-if not test -e $log
-or test (git log --oneline --max-count=1 | string split -f 1 " ") != (head -n 1 $log | string split -f 1 " ")
-    git log --oneline --name-only > $log
+# files with single match
+if test (count $files) -eq 1
+    $editor $files[1]
+else
+    if not test -e $log
+        or test (git log --oneline --max-count=1 | string split -f 1 " ") != (head -n 1 $log | string split -f 1 " ")
+        git log --oneline --name-only >$log
+    end
+
+    set data
+
+    for file in $files
+        set n (string match -g -r "(^|/)"(basename $file) < $log | count) # is (grep -E "(^|/)"(basename $file) $log | count)
+        set -a data $n" "$file
+    end
+
+    echo -s -n $data\n | sort -n -r & # debug
+
+    $editor (echo -s -n $data\n | sort -n -r | head -n 1 | string split -f 2 " ")
 end
-
-set data
-
-for file in $files
-    set n (string match -g -r "(^|/)"(basename $file) < $log | count) # is (grep -E "(^|/)"(basename $file) $log | count)
-    set -a data $n" "$file
-end
-
-echo -s -n $data\n | sort -n & # debug
-
-$editor (echo -s -n $data\n | sort -n -r | head -n 1 | string split -f 2 " ")
