3ed472233d374d87ab49aec6897b26dff5cb2315
Remi Salmon <salmon.remi@gmail.com>
2025-09-24 18:03:06 -0600

    Add scripts

---
+++ buffer-formatter.fish
@@ -0,0 +1,48 @@
+#!/usr/bin/env fish
+
+set data (cat -s - | string trim -r | string collect)
+
+switch (path extension $MICRO_FILE)
+    case ".go"
+        echo $data | gofmt
+
+    case ".fish"
+        echo $data | fish_indent
+
+    case ".json" ".jsonl"
+        echo $data | jq
+
+    case ".py"
+        # echo $data | black -
+        echo $data | isort - | black -
+
+    case ".sql"
+        if string match -q -e dbt (pwd)
+            cd (git rev-parse --show-toplevel)
+
+            source dbt_venv/bin/activate.fish
+
+            set TMP_FILE (path change-extension "" $MICRO_FILE)_tmp.sql
+
+            echo $data >$TMP_FILE
+
+            sqlfluff fix $TMP_FILE >/dev/null
+
+            set code $status
+
+            cat $TMP_FILE && rm -f $TMP_FILE
+
+            exit $code
+        else
+            echo $data | sqlfluff format -
+        end
+
+    case ".tf" ".tfvars"
+        echo $data | terraform fmt -
+
+    case ".yaml" ".yml"
+        echo $data | yamlfmt -
+
+    case "*"
+        echo $data
+end
---
+++ micro-editor.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+set -x MICRO_FILE $argv[-1]
+
+/opt/local/bin/micro $argv
