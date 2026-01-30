01b63d244488c746a19e13c895f09f1a8a6ffcfb
Remi Salmon <salmon.remi@gmail.com>
2026-01-29 21:20:25 -0700

    Update aws-sso script

--- aws-sso.fish
+++ aws-sso.fish
@@ -1,20 +1,20 @@
 #!/usr/bin/env fish
 
-if test (count $argv) -ne 1
+if not set -q $argv[1]
     echo "usage: aws-sso.fish profile" && exit 1
 end
 
-set -g AWS_PROFILE $argv[1]
+set -g profile $argv[1]
 
 function set_credentials
-    set -g credentials (aws configure export-credentials --profile $AWS_PROFILE)
+    set -g credentials (aws configure export-credentials --profile $profile)
 end
 
 if not set_credentials
-    aws sso login --profile $AWS_PROFILE && set_credentials || exit 1
+    aws sso login --profile $profile && set_credentials || exit 1
 end
 
-set -Ux AWS_REGION            (aws configure get region --profile $AWS_PROFILE)
+set -Ux AWS_REGION            (aws configure get region --profile $profile)
 set -Ux AWS_ACCESS_KEY_ID     (echo $credentials | jq -r '.AccessKeyId')
 set -Ux AWS_SECRET_ACCESS_KEY (echo $credentials | jq -r '.SecretAccessKey')
 set -Ux AWS_SESSION_TOKEN     (echo $credentials | jq -r '.SessionToken')
