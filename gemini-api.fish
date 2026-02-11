d8cecd77bd3741df454370b420c72b64a72276b8
Remi Salmon <salmon.remi@gmail.com>
2026-02-10 21:12:13 -0700

    Update gemini-api script

--- gemini-api.fish
+++ gemini-api.fish
@@ -9,7 +9,7 @@ if test -z $prompt
     exit 1
 end
 
-set data (timeout 0.5 cat | base64 -w 0)
+set data (timeout 0.5s cat | base64 -w 0)
 
 set content '{"text": "'$prompt'"}'
 
@@ -19,28 +19,34 @@ end
 
 set tic (date +%s)
 
-set response (
-    curl https://generativelanguage.googleapis.com/v1beta/models/{$model}:generateContent \
-    -s \
-    -m 300 \
-    -H 'x-goog-api-key: '$GEMINI_API_KEY \
-    -H 'Content-Type: application/json' \
-    -X POST \
-    -d '{"system_instruction": {"parts": [{"text": "'$system_instruction'"}]}, "contents": [{"parts": ['$content']}]}' \
-    | string collect
-)
+for try in (seq 3)
+    set response (
+        curl https://generativelanguage.googleapis.com/v1beta/models/{$model}:generateContent \
+        -s \
+        -m 300 \
+        -H 'x-goog-api-key: '$GEMINI_API_KEY \
+        -H 'Content-Type: application/json' \
+        -X POST \
+        -d '{"system_instruction": {"parts": [{"text": "'$system_instruction'"}]}, "contents": [{"parts": ['$content']}]}' \
+        | string collect
+    )
+
+    if test (echo $response | jq -r '.error.code') != "503"
+        break
+    else
+        sleep {$try}s
+    end
+end
 
 set toc (date +%s)
-
-set response_time (math $toc" - "$tic)
 
 if test -z $response
     set response "null"
 end
 
 duckdb (status dirname)/gemini-api.db \
-    -c 'create table if not exists logs (created_at timestamp default current_localtimestamp(), model varchar, prompt varchar, response json, response_time integer);' \
-    -c 'insert into logs (model, prompt, response, response_time) values($$'$model'$$, $$'$prompt'$$, $$'$response'$$, $$'$response_time'$$);' &
+    -c 'create table if not exists logs (created_at timestamp default current_localtimestamp(), model varchar, prompt varchar, response json, response_time_seconds integer, retry_count integer);' \
+    -c 'insert into logs (model, prompt, response, response_time_seconds, retry_count) values($$'$model'$$, $$'$prompt'$$, $$'$response'$$, $$'(math $toc - $tic)'$$, $$'(math $try - 1)'$$);' &
 
 set response (echo $response | jq -r '.candidates[0].content.parts[0].text' | string collect)
 
