f639dfe50b0fc37b0950a21563d592bde5bee81c
Remi Salmon <salmon.remi@gmail.com>
2026-02-10 21:10:54 -0700

    Update docker-clean script

--- docker-clean.fish
+++ docker-clean.fish
@@ -4,18 +4,22 @@ if not set -q argv[1]
     echo "usage: docker-clean.fish regex" && exit 1
 end
 
-set IMAGES (docker image ls | string match -e -r "^[^ ]*"$argv[1])
+command -q podman && set command podman || set command docker
 
-if test (count $IMAGES) -eq 0
+set images ($command image ls | string match -e -r '^[^ ]*'$argv[1])
+
+if test (count $images) -eq 0
     echo "no images found" && exit 0
 end
 
-docker image ls | head -n 1
+$command image ls | head -n 1
 
-echo -s {$IMAGES}\n
+string join \n $images
 
-read -P "clean [y/N]? " a
+read -p 'echo \n"REMOVE [y/N]? "' a
 
 if string match -q -i y $a
-    docker image rm -f (echo -s -n {$IMAGES}\n | string replace -a -r " +" " " | string split -f 3 " ")
+    for image in (string join \n $images | string replace -a -r " +" " " | string split -f 3 " " | uniq)
+        $command image rm -f $image
+    end
 end
