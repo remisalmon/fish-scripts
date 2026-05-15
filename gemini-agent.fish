fc58a693443cd895b8d8e3a86e138ea42175838e
Remi Salmon <salmon.remi@gmail.com>
2026-05-14 21:06:18 -0600

    Add gemini-agent script

---
+++ gemini-agent.fish
@@ -0,0 +1,28 @@
+#!/usr/bin/env fish
+
+git rev-parse || exit 1
+
+if test (count $argv) -eq 0
+    echo "usage: gemini-agent.fish PROMPT ..." && exit 1
+end
+
+set prompt (string join " " $argv)" - answer in a single json object having the path of each new or modified file as key and the full content of each new or modified file as value"
+
+set response (timeout 0.5 cat | gemini-api.fish $prompt)
+
+for k in (echo $response | jq -r '.|keys[]')
+    set v (echo $response | jq -r '."'$k'"' | string collect)
+
+    if test -e $k
+        echo "gemini-agent.fish is staging "(set_color green)$k(set_color normal)
+
+        git add $k
+    else if not test -e (path dirname $k)
+        echo "gemini-agent.fish is making "(set_color red)(path dirname $k)"/"(set_color normal)
+
+        mkdir -p (path dirname $k)
+    end
+
+    echo "gemini-agent.fish is writing to "(set_color red)$k(set_color normal)
+    echo $v >$k
+end
