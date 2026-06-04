aa6477e3afb538ee8dd5ddead7217c4a38af0d48
Remi Salmon <salmon.remi@gmail.com>
2026-06-03 21:12:06 -0600

    Update llm-context script

--- llm-context.fish
+++ llm-context.fish
@@ -2,21 +2,17 @@
 
 # inspired by https://github.com/simonw/files-to-prompt
 
+argparse n -- $argv || exit 1
+
 if test (count $argv) -eq 0
     echo "usage: llm-context.fish [-n] PATTERN ..." && exit 1
-else if test $argv[1] = -n
-    set line_numbers true
-    set args $argv[2..]
-else
-    set line_numbers false
-    set args $argv
 end
 
 if git rev-parse
-    set pattern "*"$args"*"
+    set pattern "*"$argv"*"
     set files (git ls-files $pattern)
 else
-    set pattern (string escape --style=regex -- $args | string join "|")
+    set pattern (string escape --style=regex -- $argv | string join "|")
     set files (ls -1 -a | string match -e -r $pattern)
 end
 
@@ -31,7 +27,7 @@ for file in $files
 
     echo -e "---\n"$file"\n---"
 
-    if $line_numbers
+    if set -q _flag_n
         cat -n $file | string trim -l
     else
         cat $file
