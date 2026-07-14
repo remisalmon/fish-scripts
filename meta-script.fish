7371df234cd482ac7ac0925b3fa39aaf31640a50
Remi Salmon <salmon.remi@gmail.com>
2026-07-13 18:36:07 -0600

    Update meta and micro-editor scripts

--- meta-script.fish
+++ meta-script.fish
@@ -1,5 +1,12 @@
 #!/usr/bin/env fish
 
-# ln -f -v -s gemini-cli.fish gemini
+cd (status dirname)
+
 ln -f -v -s gemini-api.fish gemini
 ln -f -v -s micro-editor.fish micro
+
+if test (uname -s) = Linux
+    ln -f -v -s (pwd)/update-linux.fish ~/update.fish
+else
+    ln -f -v -s (pwd)/update-mac.fish ~/update.fish
+end
--- micro-editor.fish
+++ micro-editor.fish
@@ -2,4 +2,8 @@
 
 set -x MICRO_FILE $argv[-1]
 
-/opt/local/bin/micro $argv
+if test (uname -s) = Linux
+    /usr/bin/micro $argv
+else
+    /opt/local/bin/micro $argv
+end
