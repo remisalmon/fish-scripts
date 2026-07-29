e5af35bc884232956df5be7a54169e276dbe2ff9
Remi Salmon <salmon.remi@gmail.com>
2026-07-29 15:52:04 -0600

    Update git-commits script

--- git-commits.fish
+++ git-commits.fish
@@ -6,14 +6,18 @@ set commits
 
 read -P "since [last Sunday]? " since
 
-if test -z $since
-    set since "last Sunday"
-end
+test -z $since && set since "last Sunday"
+
+read -P "until [today]? " until
+
+test -z $until && set until today
 
 read -P "main/master branch only [Y/n]? " main
 
+test -z $main && set main y
+
 switch (string lower $main)
-    case "" y
+    case y
         set main true
     case n
         set main false
@@ -29,13 +33,13 @@ for git_repo in ~/Work/GitHub/HotelEngine/*/
     end
 
     if $main
-        set git_branch (git branch | string match -q -r "^[* ]+master\$" && echo "master" || echo "main")
+        set git_branch (git rev-parse --abbrev-ref origin/HEAD | string replace origin/ "")
 
         git fetch origin $git_branch
 
-        set git_log (git log FETCH_HEAD --pretty={$pretty} --author={$author} --since={$since})
+        set git_log (git log FETCH_HEAD --pretty={$pretty} --author={$author} --since={$since} --until={$until})
     else
-        set git_log (git log --branches --pretty={$pretty} --author={$author} --since={$since})
+        set git_log (git log --branches --pretty={$pretty} --author={$author} --since={$since}  --until={$until})
     end
 
     for i in $git_log
